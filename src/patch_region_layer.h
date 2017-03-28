993674fcc2b8c011e4390d36b8ca746e634415ea
Jumabek <jumabek4044@gmail.com>
2017-03-28 14:55:49 -0700

    add two custom layers

---
+++ src/patch_region_layer.h
@@ -0,0 +1,20 @@
+#ifndef PATH_REGION_LAYER_H
+#define PATH_REGION_LAYER_H
+
+#include "layer.h"
+#include "network.h"
+
+typedef layer patch_region_layer;
+
+patch_region_layer make_region_layer(int batch, int h, int w, int n, int classes, int coords);
+void forward_patch_region_layer(const patch_region_layer l, network_state state);
+void backward_patch_region_layer(const patch_region_layer l, network_state state);
+void get_patch_region_boxes(layer l, int w, int h, float thresh, float **probs, box *boxes, int only_objectness, int *map);
+void resize_patch_region_layer(layer *l, int w, int h);
+
+#ifdef GPU
+void forward_patch_region_layer_gpu(const region_layer l, network_state state);
+void backward_patch_region_layer_gpu(region_layer l, network_state state);
+#endif
+
+#endif
