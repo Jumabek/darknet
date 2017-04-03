71d3b5413835e2200a86c09794e4473f0b7bf21c
Jumabek <jumabek4044@gmail.com>
2017-04-02 18:06:07 -0700

    add patch_region_layer

--- build/darknet/x64/darknet_demo_voc.cmd
+++ build/darknet/x64/darknet_demo_voc.cmd
@@ -1,4 +1,4 @@
-darknet.exe detector demo data/voc.data yolo-voc.cfg yolo-voc.weights test.mp4 -i 0
+darknet.exe detector demo data/voc.data cfg/yolo-voc.cfg backup/yolo-voc/yolo-voc_200.weights F:\dataset\demo_vids\ADL-Rundle-3.mp4 -i 0 -thresh 0.0005
 
 
 pause
--- src/patch_region_layer.h
+++ src/patch_region_layer.h
@@ -6,15 +6,15 @@
 
 typedef layer patch_region_layer;
 
-patch_region_layer make_region_layer(int batch, int h, int w, int n, int classes, int coords);
+patch_region_layer make_patch_region_layer(int batch, int h, int w, int classes);
 void forward_patch_region_layer(const patch_region_layer l, network_state state);
 void backward_patch_region_layer(const patch_region_layer l, network_state state);
 void get_patch_region_boxes(layer l, int w, int h, float thresh, float **probs, box *boxes, int only_objectness, int *map);
 void resize_patch_region_layer(layer *l, int w, int h);
 
 #ifdef GPU
-void forward_patch_region_layer_gpu(const region_layer l, network_state state);
-void backward_patch_region_layer_gpu(region_layer l, network_state state);
+void forward_patch_region_layer_gpu(const patch_region_layer l, network_state state);
+void backward_patch_region_layer_gpu(patch_region_layer l, network_state state);
 #endif
 
 #endif
