91f72f6101026979244c19e883569e258a5f4e2c
Jumabek <jumabek4044@gmail.com>
2017-06-02 13:54:45 -0700

    Etc

--- build/darknet/x64/darknet_demo_voc.cmd
+++ build/darknet/x64/darknet_demo_voc.cmd
@@ -1,4 +1,4 @@
-darknet.exe detector demo data/voc.data cfg/yolo-voc.cfg backup/yolo-voc/yolo-voc_200.weights F:\dataset\demo_vids\ADL-Rundle-3.mp4 -i 0 -thresh 0.0005
+darknet.exe detector demo data/voc.data cfg/yolo-voc-test-896.cfg weights/yolo-voc.weights F:/vms.mp4 -i 0 -thresh 0.4
 
 
 pause
